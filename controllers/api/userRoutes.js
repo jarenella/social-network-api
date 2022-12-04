2cd42ac2523dd0252ae040550f6083cb9cf0dd06
jarenella <jamesa122333@gmail.com>
2022-12-03 18:45:56 -0800

    all routes in userRoutes.js now properly update and retrieve information from db

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,33 +1,43 @@
 const router = require('express').Router();
+const User = require("../../models/User");
 
 // /users get route
 // sends data of all users in database
-router.get('/', (req, res) => {
-    res.send("hi user route");
+router.get('/', async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
 });
 
 // /users/:id get route
 // sends data for one user by their id
-router.get('/:id', (req, res) => {
-    res.send(`u a gonna geta user by their id, which is ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    const user = await User.findOne({_id: `${req.params.id}`});
+    res.send(user);
 })
 
 // /users post route
 // creates a new user
-router.post('/', (req, res) => {
-    res.send("ur gonna add a new user through this route")
+router.post('/', async (req, res) => {
+    const newUser = await  User.create([
+        { username: req.body.username, email: req.body.email }
+    ]);
+    res.send(newUser);
 })
 
 // /users put route
 // updates a users info
-router.put('/', (req, res) => {
-    res.send("update user info")
+router.put('/:id', async (req, res) => {
+    const newInfo = {username: req.body.username, email: req.body.email};
+    const userToUpdate = await User.findOne({_id: `${req.params.id}`});
+    const updatedUser = await userToUpdate.updateOne(newInfo);
+    res.send("Put request recieved.");
 })
 
 // /users delete route
 // deletes a user from the database
-router.delete('/', (req, res) => {
-    res.send('delete user');
+router.delete('/:id', async (req, res) => {
+    const user = await User.deleteOne({id: req.params.id})
+    res.send("Delete request recieved.");
 })
 
 module.exports = router;
